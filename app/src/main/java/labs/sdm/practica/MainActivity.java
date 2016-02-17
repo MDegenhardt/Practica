b77b2506004b2665931c5f1db4db6b92c8da8d27
Markus Degenhardt <degenhardt.markus@gmail.com>
2016-02-18 00:26:08 +0100

    UI init

--- app/src/main/java/labs/sdm/practica/MainActivity.java
+++ app/src/main/java/labs/sdm/practica/MainActivity.java
@@ -1,7 +1,9 @@
 package labs.sdm.practica;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dashboardButtonClicked(View view){
+
+        Intent intent = null;
+        switch(view.getId()){
+            case R.id.bPlay :
+                intent = new Intent(this,PlayActivity.class);
+                break;
+            case R.id.bScores :
+                intent = new Intent(this,ScoresActivity.class);
+                break;
+            case R.id.bSettings :
+                intent = new Intent(this,SettingsActivity.class);
+                break;
+
+        }
+        startActivity(intent);
     }
 }
