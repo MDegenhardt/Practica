3dca4aa1b0ddba5af0830f2bd5e4ed9a39fa1431
Markus Degenhardt <degenhardt.markus@gmail.com>
2016-02-20 23:58:42 +0100

    UI Tabhost start

--- app/src/main/java/labs/sdm/practica/ScoresActivity.java
+++ app/src/main/java/labs/sdm/practica/ScoresActivity.java
@@ -1,7 +1,8 @@
 package labs.sdm.practica;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class ScoresActivity extends AppCompatActivity {
 
@@ -9,5 +10,31 @@ public class ScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+
+        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setup();
+
+        TabHost.TabSpec spec = host.newTabSpec(("TAB1"));
+
+        //Tab indicator specified as Label and Icon
+
+        spec.setIndicator("Label1",getResources().getDrawable(R.mipmap.ic_launcher));
+
+        spec.setContent(R.id.linearLayout);
+
+        host.addTab(spec);
+
+        spec = host.newTabSpec(("TAB2"));
+
+        //Tab indicator specified as Label and Icon
+
+        spec.setIndicator("Label2",getResources().getDrawable(R.mipmap.ic_launcher));
+
+        spec.setContent(R.id.linearLayout2);
+
+        host.addTab(spec);
+
+
+        host.setCurrentTab(1);
     }
 }
